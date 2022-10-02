83ed5a192c6b983836ed77fadc85749adab1c13d
tanerijun <tanerivince@gmail.com>
2022-10-02 16:42:56 +0800

    Edit email icon

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,8 +6,9 @@
           <a v-if="$config.email" target="_blank" rel="noreferrer" :href="`mailto:${$config.email}`"
             class="text-gray-400 hover:text-gray-300">
             <span class="sr-only">Email</span>
-            <svg class="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 8 6">
-              <path d="m0 0h8v6h-8zm.75 .75v4.5h6.5v-4.5zM0 0l4 3 4-3v1l-4 3-4-3z" />
+            <svg class="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 18 18">
+              <path
+                d="M17.388,4.751H2.613c-0.213,0-0.389,0.175-0.389,0.389v9.72c0,0.216,0.175,0.389,0.389,0.389h14.775c0.214,0,0.389-0.173,0.389-0.389v-9.72C17.776,4.926,17.602,4.751,17.388,4.751 M16.448,5.53L10,11.984L3.552,5.53H16.448zM3.002,6.081l3.921,3.925l-3.921,3.925V6.081z M3.56,14.471l3.914-3.916l2.253,2.253c0.153,0.153,0.395,0.153,0.548,0l2.253-2.253l3.913,3.916H3.56z M16.999,13.931l-3.921-3.925l3.921-3.925V13.931z" />
             </svg>
           </a>
           <a v-if="$config.social.facebook" target="_blank" rel="noreferrer"
